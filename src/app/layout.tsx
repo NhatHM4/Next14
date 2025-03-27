951d18f4d8d4db48dcfa5ed59d6df90054e19f57
NhatHM4 <haminhnhat711@gmail.com>
2025-03-27 20:00:41 +0700

    video #206

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import StyledComponentsRegistry from '@/lib/antd.registry'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <StyledComponentsRegistry>
+
+        <body className={inter.className}>{children}</body>
+      </StyledComponentsRegistry>
     </html>
   )
 }
